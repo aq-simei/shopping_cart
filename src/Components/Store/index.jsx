80a23946b6e30dae735bb798f0df5dd1f539d179
Antonio Simei <antonio.simei@codeminer42.com>
2022-03-15 12:58:47 -0300

    Change initial products declarion to const outside the component

--- src/Components/Store/index.jsx
+++ src/Components/Store/index.jsx
@@ -1,30 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ProductsList } from "../ProductsList";
 import { Cart } from "../Cart";
 import { Button } from "../Button";
 
+const initialProducts = [
+  {
+    id: 1,
+    name: 'Caneta',
+    description: 'R$2,00'
+  },
+  {
+    id: 2,
+    name: 'Pacote folha sulfite',
+    description: 'R$10,00'
+  },
+  {
+    id: 3,
+    name:'Lápis',
+    description: 'R$0,50'
+  }
+]
 export function Store() {
-  let initialProducts = [
-    {
-      id: 1,
-      name: 'Caneta',
-      description: 'R$2,00'
-    },
-    {
-      id: 2,
-      name: 'Pacote folha sulfite',
-      description: 'R$10,00'
-    },
-    {
-      id: 3,
-      name:'Lápis',
-      description: 'R$0,50'
-    }
-  ]
 
   const [showComponents, setShowComponents] = useState("");
-  const [products, setProducts] = useState(initialProducts)
+  const [products, setProducts] = useState(initialProducts);
+  const [cartItems, setCartItems] = useState([]);
+  
 
+  const addToCart = (params) => {
+    console.log('Add to cart button is working')
+    setCartItems([...cartItems, params])
+  }
   return (
     <>
       <Button
@@ -35,7 +41,7 @@ export function Store() {
         name="Cart" 
         onClick={() => setShowComponents("Cart")} 
       />
-      {showComponents === "Products" && <ProductsList products={products}/>}
+      {showComponents === "Products" && <ProductsList products={products} addToCart={addToCart}/>}
       {showComponents === "Cart" && <Cart />}
     </>
   );
