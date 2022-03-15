1f8d306174b8826b9eb4106ad4165b998e49f33d
Antonio Simei <antonio.simei@codeminer42.com>
2022-03-15 13:07:16 -0300

    Add <ul> wrapper to <li>

--- src/Components/ProductsList/index.jsx
+++ src/Components/ProductsList/index.jsx
@@ -1,12 +1,14 @@
 export function ProductsList({products}) {
   return (
-    products.map((product)=>{
+    <ul>
+    {products.map((product)=>{
       return(
         <li key={product.id}>
           <span>{product.name}</span>
           <span>{product.description}</span>
         </li>
       )
-    })
+    })}
+    </ul>
   )
 }
