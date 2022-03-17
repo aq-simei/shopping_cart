fb05e3a4e20db92c259933a13bc82a346025f996
Antonio Simei <antonio.simei@codeminer42.com>
2022-03-16 21:35:43 -0300

    Remove unnecessary key from CartItem

--- src/Components/CartItem/index.jsx
+++ src/Components/CartItem/index.jsx
@@ -2,7 +2,7 @@ import { Button } from "../Button";
 
 export const CartItem = ({ addedProduct, onHandleRemoveItem }) => {
   return (
-    <li key={addedProduct.id}>
+    <li>
       <span>{addedProduct.name}</span>
       <span>{addedProduct.description}</span>
       <Button name="Remove from cart" onClick={() => {}} />
