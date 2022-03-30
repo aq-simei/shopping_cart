2951d41919b068ea84a604c1f6e6f359d5c03eb4
Antonio Simei <antonio.simei@codeminer42.com>
2022-03-30 10:27:11 -0300

    Create a new theme

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/system";
 import { Store } from "./Components/Store";
 import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../src/theme";
+
+const theme = { theme };
 
 export function App() {
   const theme = createTheme();
