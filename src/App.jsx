b96fcb9a8662043b366249c7679bab17975103ff
Antonio Simei <antonio.simei@codeminer42.com>
2022-03-30 13:15:12 -0300

    Test stylization with css function

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,25 @@ import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "styled-components";
 import { Store } from "./Components/Store";
 import { theme } from "./theme";
+import css from "@styled-system/css";
 
 export function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Store />
+      <Card>
+        <p>Esse é o teste com a função css</p>
+      </Card>
     </ThemeProvider>
   );
 }
+const Card = (props) => (
+  <div
+    {...props}
+    css={css({
+      background: `${theme.colors.black}`,
+      color: `${theme.colors.white}`,
+    })}
+  />
+);
